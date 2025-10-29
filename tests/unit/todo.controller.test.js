d9f799ad67b6d88c7ac62735b0b06a49992daf10
CarlRobertMots <robertm6ts@gmail.com>
2025-10-29 09:21:58 +0200

    Created TodoController.getTodoById unit test and integration tests

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -6,6 +6,7 @@ const allTodos = require('../mock-data/all-todos.json')
 
 TodoModel.create = jest.fn()
 TodoModel.find = jest.fn()
+TodoModel.findById = jest.fn()
 
 let req, res, next
 beforeEach(() =>{
@@ -63,6 +64,36 @@ describe("TodoController.getTodos", () => {
         expect(res._getJSONData()).toStrictEqual(allTodos)
     })
     it("should handle errors in getTodos", async () => {
-        
+
     })
 })
+describe("TodoController.getTodoById", () => {
+    it("should have a getTodoById", () => {
+        expect (typeof TodoController.getTodoById).toBe("function")
+    })
+    it("should call TodoModel.findById with route parameters", async () => {
+        req.params.todoId = "68f8c423ecf8d646e7f24672"
+        await TodoController.getTodoById(req, res, next)
+        expect(TodoModel.findById).toHaveBeenCalledWith("68f8c423ecf8d646e7f24672")
+    })
+    it("should return json body and response code 200", async () => {
+        TodoModel.findById.mockReturnValue(newTodo)
+        await TodoController.getTodoById(req, res, next)
+        expect(res.statusCode).toBe(200)
+        expect(res._getJSONData()).toStrictEqual(newTodo)
+    })
+    it("should do error handling", async () => {
+        const errorMessage = { message: "error finding todoModel" }
+        const rejectedPromise = Promise.reject(errorMessage)
+        TodoModel.findById.mockReturnValue(rejectedPromise)
+        await TodoController.getTodoById(req, res, next)
+        expect(next).toHaveBeenCalledWith(errorMessage)
+    })
+    it("should return 404 when item doesnt exist", async () =>{
+        TodoModel.findById.mockReturnValue(null)
+        await TodoController.getTodoById(req, res, next)
+        expect(res.statusCode).toBe(404)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+    
+})  
